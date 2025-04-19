f42bc1a9e147695b8d038b3969e1cc088cb1cd6d
yannickbohrer <bohreryannick@gmail.com>
2025-04-19 14:43:41 +0200

    correct test file name in global var

--- rtc/include/CLI.hpp
+++ rtc/include/CLI.hpp
@@ -30,7 +30,7 @@ inline const std::string styles = "styles";
 // rtc provided templates
 inline const std::string componentTemplate = "component.txt";
 inline const std::string jsTestTemplate = "component-test-js.txt";
-inline const std::string tsTestTemplate = "component-test-js.txt";
+inline const std::string tsTestTemplate = "component-test-ts.txt";
 inline const std::string cssTemplate = "component-styles-css.txt";
 
 }  // namespace Tokens
